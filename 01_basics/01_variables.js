e20c8bd023af43184f525570a09286f9ab4a8b6f
sudhanshu-raj-eksu <sudhanshuraj905@gmail.com>
2024-08-19 07:40:55 +0000

    any message

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,25 @@
+const accountId=705056
+let accountEmail="sudhanshu@gmail.com"
+var accountPassword="123456"
+accountCity ="Bangalore"
+let accountState;
+
+//accountId=2//
+
+accountEmail="sudha@gmail.com"
+accountPassword="1234"
+accountCity="Bangalore"
+
+console.log(accountId);
+
+/*
+Prefer not to use var because
+of issues in block scope and functional scope
+
+
+*/
+
+
+
+console.table([accountId,accountEmail,accountPassword,accountPassword,accountCity,accountState])
+
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict";//treat all JS code as new version
+
+//alert(3+3)//we are using nodejs,not browser
+
+console.log(3+3)//code readability should be high
+
+console.log("sudhanshu")
+
+let name="sudhanshu"
+let age=19
+let isLoggedIn=false
+let state;
+
+//number=>2 to power 53
+//bigint
+//string =>""
+//boolen =>true/false
+//null=>standalone value
+//undefined=>
+//symbol =>unique
+
+
+//object
+
+console.log(typeof undefined);//undefined
+console.log(typeof null);//object
+
+
